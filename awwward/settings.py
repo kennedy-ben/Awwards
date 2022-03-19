fbe5ddbef2040d62ae8670fb9d238294f3c9743b
kennedy bernard <makuanikennedy@gmail.com>
2022-03-19 19:34:54 +0300

    created database

--- awwward/settings.py
+++ awwward/settings.py
@@ -81,8 +81,10 @@ WSGI_APPLICATION = 'awwward.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'awwards',
+        'USER': 'kennedy',
+    'PASSWORD': 'awwards9090',
     }
 }
 
